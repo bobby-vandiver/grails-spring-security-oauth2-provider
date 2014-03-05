d8afe218aacd4c9f741ae481726e7339b7c2fd26
Bobby Vandiver <bobby.vandiver88@gmail.com>
2014-03-04 21:36:01 -0500

    Register sparklr clients in BootStrap

--- examples/grails-sparklr/grails-app/conf/BootStrap.groovy
+++ examples/grails-sparklr/grails-app/conf/BootStrap.groovy
@@ -17,10 +17,65 @@ class BootStrap {
         ).save(flush:true)
         UserRole.create(user, role, true)
 
-        GormOAuth2Client client = new GormOAuth2Client(
-                clientId: 'clientId',
-                clientSecret: 'clientSecret',
-                authorizedGrantTypes: ['password']
+        new GormOAuth2Client(
+                clientId: 'my-trusted-client',
+                authorizedGrantTypes: ['password', 'authorization_code', 'refresh_token', 'implicit'],
+                authorities: ['ROLE_CLIENT', 'ROLE_TRUSTED_CLIENT'],
+                scopes: ['read', 'write', 'trust'],
+                accessTokenValiditySeconds: 60
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'my-trusted-client-with-secret',
+                clientSecret: 'somesecret',
+                authorizedGrantTypes: ['password', 'authorization_code', 'refresh_token', 'implicit'],
+                authorities: ['ROLE_CLIENT', 'ROLE_TRUSTED_CLIENT'],
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'my-client-with-secret',
+                clientSecret: 'secret',
+                authorizedGrantTypes: ['client_credentials'],
+                authorities: ['ROLE_CLIENT'],
+                scopes: ['read']
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'my-less-trusted-client',
+                authorizedGrantTypes: ['authorization_code', 'implicit'],
+                authorities: ['ROLE_CLIENT']
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'my-less-trusted-autoapprove-client',
+                authorizedGrantTypes: ['implicit'],
+                authorities: ['ROLE_CLIENT'],
+                scopes: ['read', 'write', 'trust']
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'my-client-with-registered-redirect',
+                authorizedGrantTypes: ['authorization_code', 'client_credentials'],
+                authorities: ['ROLE_CLIENT'],
+                redirectUris: ['http://anywhere?key=value'],
+                scopes: ['read', 'trust']
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'my-untrusted-client-with-registered-redirect',
+                authorizedGrantTypes: ['authorization_code'],
+                authorities: ['ROLE_CLIENT'],
+                redirectUris: ['http://anywhere'],
+                scopes: ['read']
+        ).save(flush: true)
+
+        new GormOAuth2Client(
+                clientId: 'tonr',
+                clientSecret: 'secret',
+                resourceIds: ['sparklr'],
+                authorizedGrantTypes: ['authorization_code', 'implicit'],
+                authorities: ['ROLE_CLIENT'],
+                scopes: ['read', 'write']
         ).save(flush: true)
     }
 
